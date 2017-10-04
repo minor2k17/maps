650eadd2c67b58ed119263b507f1245944c79c36
Shaurya <shauryauppal00111@gmail.com>
2017-10-05 00:43:32 +0530

    Maps satelite view add with few controls(like scroll,tilt,zoom) of maps modified

---
+++ app/src/main/java/com/example/shaur/nimblemaps/MapsActivity.java
@@ -0,0 +1,104 @@
+package com.example.shaur.nimblemaps;
+
+import android.support.v4.app.FragmentActivity;
+import android.os.Bundle;
+import android.widget.Toast;
+
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.BitmapDescriptorFactory;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.Marker;
+import com.google.android.gms.maps.model.MarkerOptions;
+
+import static com.example.shaur.nimblemaps.R.id.map;
+
+public class MapsActivity extends FragmentActivity implements OnMapReadyCallback,GoogleMap.OnMarkerClickListener{
+
+    private GoogleMap mMap;
+    private static final LatLng PERTH = new LatLng(-31.952854, 115.857342);
+    private static final LatLng SYDNEY = new LatLng(-33.87365, 151.20689);
+    private static final LatLng BRISBANE = new LatLng(-27.47093, 153.0235);
+    private Marker mPerth;
+    private Marker mSydney;
+    private Marker mBrisbane;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_maps);
+        // Obtain the SupportMapFragment and get notified when the map is ready to be used.
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
+                .findFragmentById(map);
+        mapFragment.getMapAsync(this);
+        if (map!=null){
+            Marker Temple = map.addMarker(new MarkerOptions().position(Temple)
+                    .title("Hamburg"));
+            Marker OAT = map.addMarker(new MarkerOptions()
+                    .position(OAT)
+                    .title("OAT")
+                    .snippet("Kiel is cool")
+                    .icon(BitmapDescriptorFactory
+                            .fromResource(R.drawable.ic_launcher)));
+        }
+
+
+    }
+
+
+    /**
+     * Manipulates the map once available.
+     * This callback is triggered when the map is ready to be used.
+     * This is where we can add markers or lines, add listeners or move the camera. In this case,
+     * we just add a marker near Sydney, Australia.
+     * If Google Play services is not installed on the device, the user will be prompted to install
+     * it inside the SupportMapFragment. This method will only be triggered once the user has
+     * installed Google Play services and returned to the app.
+     */
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        mMap = googleMap;
+
+        mMap.setMapType(GoogleMap.MAP_TYPE_SATELLITE);
+        // Add a marker in JIIT and move the camera
+        LatLng JIIT = new LatLng(28.630509, 77.372091);
+        mMap.addMarker(new MarkerOptions().position(JIIT).title("Marker in JIIT"));
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(JIIT));
+        // Add some markers to the map, and add a data object to each marker.
+        mPerth = mMap.addMarker(new MarkerOptions()
+                .position(PERTH)
+                .title("Perth"));
+        mPerth.setTag(0);
+
+        mSydney = mMap.addMarker(new MarkerOptions()
+                .position(SYDNEY)
+                .title("Sydney"));
+        mSydney.setTag(0);
+
+        mBrisbane = mMap.addMarker(new MarkerOptions()
+                .position(BRISBANE)
+                .title("Brisbane"));
+        mBrisbane.setTag(0);
+
+        // Set a listener for marker click.
+        mMap.setOnMarkerClickListener(this);
+        @Override
+        public boolean onMarkerClick( final Marker marker ) {
+
+            // Retrieve the data from the marker.
+            Integer clickCount = (Integer) marker.getTag();
+
+            // Check if a click count was set, then display the click count.
+            if (clickCount != null) {
+                clickCount = clickCount + 1;
+                marker.setTag(clickCount);
+                Toast.makeText(this,
+                        marker.getTitle() +
+                                " has been clicked " + clickCount + " times.",
+                        Toast.LENGTH_SHORT).show();
+            }
+        }
+    }
+}
